ac7a5156b35ea5e4445cbb20ffed16d3b73d5580
ShahMir <chand14ok@gmail.com>
2026-09-14 21:57:50 -0700

    Reediting circle schema

--- database/schemas/circle.schema.ts
+++ database/schemas/circle.schema.ts
@@ -5,6 +5,7 @@ const CircleSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true, trim: true },
     goalAmount: { type: Number, default: 0 },
+    durationMonths: { type: Number, default: 0 },
     currency: { type: String, default: "PKR" },
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
